0148639ba527bf4ce10c3abd21bbc12b72e58ecb
Phillip Cloud <cpcloud@gmail.com>
2021-05-01 11:07:11 -0400

    Add needed error variants

--- src/error.rs
+++ src/error.rs
@@ -11,4 +11,19 @@ pub(crate) enum Error {
 
     #[error("failed to get ctrlc handler")]
     SetHandler(#[source] ctrlc::Error),
+
+    #[error("failed to get stdout in raw mode")]
+    GetStdoutInRawMode(#[source] std::io::Error),
+
+    #[error("failed to create terminal object")]
+    CreateTerminal(#[source] std::io::Error),
+
+    #[error("failed to parse number from string: {1}")]
+    ParseNum(#[source] std::num::ParseIntError, String),
+
+    #[error("value must be greater than zero")]
+    GetNonZeroValue,
+
+    #[error("failed to convert u32 to usize")]
+    ConvertU32ToUsize(#[source] std::num::TryFromIntError),
 }
